625fddd07cfb6e4a209e906a6c5d2fcc7988a6de
PTP <parthparikh28@gmail.com>
2019-05-26 11:48:10 +0530

    Added timer

--- controller.js
+++ controller.js
@@ -1,12 +1,75 @@
 var video = document.getElementById("videoPlayer");
-var btn = document.getElementById("playButton");
+var audio = document.getElementById("audioPlayer");
+var button = document.getElementById("playButton");
+var buttonImage = document.getElementById("buttonState");
+var minutes = 1;
+var seconds = 60;
+var timeString = {minutes: 0 , seconds: 0};
 
-function myFunction() {
+function startTimer() {
   if (video.paused) {
     video.play();
-    btn.value = "Pause";
+    audio.play();
+    button.value = "Pause";
+    buttonImage.src = "./svg/pause.svg";
+    startCountdown();
   } else {
     video.pause();
-    btn.value = "Play";
+    audio.pause();
+    buttonImage.src = "./svg/play.svg";
+    button.value = "Play";
+    stopCountdown();
   }
+}
+
+function setMinutes(timerValue)
+{
+  minutes = timerValue;
+  seconds = minutes * 60;
+  timeString.minutes = minutes;
+  timeString.seconds = 0;
+  displayTime();
+} 
+
+function startCountdown() 
+{ 
+  timer = setInterval('decrementCounter()', 1000);
+}
+
+function stopCountdown() 
+{ 
+  clearInterval(timer);
+  video.pause();
+  audio.pause();
+  buttonImage.src = "./svg/play.svg";
+  button.value = "Play";
+}
+  
+function decrementCounter()
+{
+  if(seconds <= 0)
+  {
+    alert("TIME UP");
+    timeString.minutes = 00;
+    timeString.seconds = 01;
+    seconds = 1;
+    stopCountdown();
+  }
+
+  timeString.minutes = Math.floor(seconds / 60);
+
+  if(seconds % 60 == 0)
+  {
+    timeString.seconds = 60;
+    timeString.minutes--;
+  }
+  timeString.seconds--;
+  seconds--;
+
+  displayTime();
+}
+
+function displayTime()
+{
+  document.getElementById("time").innerHTML = timeString.minutes + ":" + timeString.seconds;
 }
