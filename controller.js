1dc044ad72e6d761dfa3c5eb9dfc9166423af752
PTP <parthparikh28@gmail.com>
2019-05-31 11:50:14 +0530

    Updated UI and added another theme button

--- controller.js
+++ controller.js
@@ -11,12 +11,12 @@ function startTimer() {
     video.play();
     audio.play();
     button.value = "Pause";
-    buttonImage.src = "./svg/pause.svg";
+    buttonImage.src = "./images/pause.svg";
     startCountdown();
   } else {
     video.pause();
     audio.pause();
-    buttonImage.src = "./svg/play.svg";
+    buttonImage.src = "./images/play.svg";
     button.value = "Play";
     stopCountdown();
   }
@@ -29,22 +29,22 @@ function setMinutes(timerValue)
   timeString.minutes = minutes;
   timeString.seconds = 0;
   displayTime();
-} 
+}
 
-function startCountdown() 
-{ 
+function startCountdown()
+{
   timer = setInterval('decrementCounter()', 1000);
 }
 
-function stopCountdown() 
-{ 
+function stopCountdown()
+{
   clearInterval(timer);
   video.pause();
   audio.pause();
-  buttonImage.src = "./svg/play.svg";
+  buttonImage.src = "./images/play.svg";
   button.value = "Play";
 }
-  
+
 function decrementCounter()
 {
   if(seconds <= 0)
@@ -72,4 +72,26 @@ function decrementCounter()
 function displayTime()
 {
   document.getElementById("time").innerHTML = timeString.minutes + ":" + timeString.seconds;
+}
+
+function changeTheme(themeNumber)
+{
+  if(!video.paused)
+  {
+    video.pause();
+    audio.pause();
+    stopCountdown();
+  }
+  switch (themeNumber) {
+      case 1:
+       video.src = "./video/rain.mp4";
+       audio.src = "./sounds/rain.mp3";
+      break;
+      case 2:
+      video.src = "./video/beach.mp4";
+      audio.src = "./sounds/beach.mp3";
+      break;
+    default:
+
+  }
 }
